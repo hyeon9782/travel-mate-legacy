e85875c0c50c7e2ae42b457282bed7acbde657fb
hyeon9782 <hyeon9782@gmail.com>
2023-03-15 23:12:12 +0900

    Feat: 스켈레톤 UI 컴포넌트 개발

---
+++ client/src/components/common/Skeleton.jsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+const Skeleton = () => {
+    return (
+        <SkeletonBlock>
+
+        </SkeletonBlock>
+    )
+}
+
+const SkeletonBlock = styled.div`
+    @keyframes loading {
+        0% {
+            transform: translateX(0);
+        }
+        50%,
+        100% {
+            transform: translateX(460px);
+        }
+    }
+
+    
+`
+
+export default Skeleton;
