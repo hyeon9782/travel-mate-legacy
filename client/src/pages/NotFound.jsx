04e8556781a70efe6c18777682d6ed1610236372
hyeon9782 <hyeon9782@gmail.com>
2023-03-18 10:47:02 +0900

    Feat: 에러페이지 컴포넌트 개발

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -3,6 +3,7 @@ import Container from "../components/common/Container";
 const NotFound = () => {
     return (
         <Container>
+            <div>dds</div>
             NotFound
         </Container>
     )
